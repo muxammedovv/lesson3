46c214eb0830961e67a5eabae3f8298da88c08f7
orifjon12 <loxiya04@gmail.com>
2026-07-18 15:38:07 +0500

    meniki

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import "./Navbar.css";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Navbar = () => {
-  return <div>Orifjon</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <img src="./navbar.svg" alt="" />
+          <div className="nav-wrap">
+            <div className="nav-medium">
+              <input type="text" placeholder="Serach" />
+            </div>
+            <div className="nav-icon-box">
+              <a href="">
+                {" "}
+                <IoSearchSharp className="icon" />
+              </a>
+            </div>
+          </div>
+          <p>Your Account</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
